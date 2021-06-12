da8bbbdfdbc215ad043dcd257b23d000e093b74e
JustTory <johnnynathany@gmail.com>
2021-06-13 00:25:33 +0700

    threading update for server & client

--- client1.py
+++ client1.py
@@ -1,6 +1,8 @@
 import socket
 from tkinter import *
+# from tkinter.ttk import Progressbar
 from tkinter import messagebox
+from threading import Thread
 
 def send(msg):
     s.sendall(bytes(msg, "utf8"))
@@ -24,6 +26,87 @@ def frameManager(serverResponse):
     elif (serverResponse == "syntax error"):
         messagebox.showerror("Error", "Username or password can't be empty")
 
+def showFrame(frame):
+    frame.tkraise()
+
+# def setUpPBFrame():
+#     progressBar = Progressbar(root, orient='horizontal', mode='indeterminate', length=200)
+#     progressBar.grid(column=0, row=0, columnspan=2, padx=10, pady=20)
+#     progressBar.start()
+
+def setUpChooseSVFrame():
+    Label(chooseSVFrame, text="SERVER IP").pack(pady=20)
+    Label(chooseSVFrame, text="Input server's IP").pack()
+    serverIPEntry = Entry(chooseSVFrame)
+    serverIPEntry.bind("<Return>", (lambda event: connectThread(serverIPEntry)))
+    serverIPEntry.pack()
+    serverIPEntry.focus()
+
+    Button(chooseSVFrame, text="Select", height="1", width="10", command=lambda:connectThread(serverIPEntry)).pack(pady=10)
+
+def setUpSignInFrame():
+    Label(signInFrame, text="SIGN IN").pack(pady=20)
+    Label(signInFrame, text="Username").pack()
+    usernameEntry = Entry(signInFrame)
+    usernameEntry.bind("<Return>", (lambda event: sendUserInfo(usernameEntry, passwordEntry, "signin")))
+    usernameEntry.pack()
+
+    Label(signInFrame, text="Password").pack(pady=(10,0))
+    passwordEntry = Entry(signInFrame, show= '*')
+    passwordEntry.bind("<Return>", (lambda event: sendUserInfo(usernameEntry, passwordEntry, "signin")))
+    passwordEntry.pack()
+
+
+    Button(signInFrame, text="Login", width=10, height=1, command=lambda: sendUserInfo(usernameEntry, passwordEntry, "signin")).pack(pady=(20,10))
+    Button(signInFrame, text="Don't have an account? Sign up", width=30, height=1, command=lambda: showFrame(signUpFrame)).pack()
+
+def setUpSignUpFrame():
+    Label(signUpFrame, text="CREATE ACCOUNT").pack(pady=20)
+    Label(signUpFrame, text="Username").pack()
+    usernameEntry = Entry(signUpFrame)
+    usernameEntry.pack()
+
+    Label(signUpFrame, text="Password").pack(pady=(10,0))
+    passwordEntry = Entry(signUpFrame, show= '*')
+    passwordEntry.pack()
+
+    Button(signUpFrame, text="Create account", width=15, height=1, command=lambda: sendUserInfo(usernameEntry, passwordEntry, "signup")).pack(pady=(20,10))
+    Button(signUpFrame, text="Already have an account? Sign in", width=30, height=1, command=lambda: showFrame(signInFrame)).pack()
+
+def setUpMainMenuFrame():
+    Label(mainMenuFrame, text="MAIN MENU").pack()
+    Button(mainMenuFrame, text="List all cities", width=15, height=1).pack()
+    Button(mainMenuFrame, text="Select a city", width=15, height=1).pack()
+
+def connectThread(entry):
+    # global threadConnect
+    threadConnect = Thread(target=connectServer, args=(entry,))
+    threadConnect.daemon = True
+    threadConnect.start()
+    # chooseSVFrame.after(100, lambda: checkThreadStatus(threadConnect))
+
+# def checkThreadStatus(thread, frame):
+#     if thread.is_alive() == False:
+#         frame.grid_forget()
+#     else
+#         checkThreadStatus(thread)
+
+def connectServer(entry):
+    host = entry.get()
+    serverAddr = (host, PORT)
+    try:
+        s.connect(serverAddr)
+        print('Connected to server: ' + str(serverAddr))
+        showFrame(signInFrame)
+    except:
+        print("Could not find server's IP or request timeout")
+        messagebox.showerror("Error", "Could not find server's IP or request timeout")
+
+# def sendUserInfoThread(usernameEntry, passwordEntry, type):
+#     threadSend = Thread(target=sendUserInfo, args=(usernameEntry, passwordEntry, type,))
+#     threadSend.daemon = True
+#     threadSend.start()
+
 def sendUserInfo(usernameEntry, passwordEntry, type):
     username = usernameEntry.get()
     usernameEntry.delete(0, 'end')
@@ -34,54 +117,7 @@ def sendUserInfo(usernameEntry, passwordEntry, type):
     serverResponse = receive()
     frameManager(serverResponse)
 
-def showFrame(frame):
-    frame.tkraise()
-
-def setUpChooseSVFrame():
-    Label(chooseSVFrame, text="Input server's IP").pack()
-    serverIPEntry = Entry(chooseSVFrame)
-    serverIPEntry.pack()
-    Button(chooseSVFrame, text="Select", height="1", width="10", command=lambda: connectServer(serverIPEntry)).pack()
-
-def setUpSignInFrame():
-    Label(signInFrame, text="SIGN IN").pack()
-    Label(signInFrame, text="Username").pack()
-    usernameEntry = Entry(signInFrame)
-    usernameEntry.pack()
-    Label(signInFrame, text="Password").pack()
-    passwordEntry = Entry(signInFrame, show= '*')
-    passwordEntry.pack()
-    Button(signInFrame, text="Login", width=10, height=1, command=lambda: sendUserInfo(usernameEntry, passwordEntry, "signin")).pack()
-    Button(signInFrame, text="Don't have an account? Sign up", width=30, height=1, command=lambda: showFrame(signUpFrame)).pack()
-
-def setUpSignUpFrame():
-    Label(signUpFrame, text="CREATE ACCOUNT").pack()
-    Label(signUpFrame, text="Username").pack()
-    usernameEntry = Entry(signUpFrame)
-    usernameEntry.pack()
-    Label(signUpFrame, text="Password").pack()
-    passwordEntry = Entry(signUpFrame, show= '*')
-    passwordEntry.pack()
-    Button(signUpFrame, text="Create account", width=15, height=1, command=lambda: sendUserInfo(usernameEntry, passwordEntry, "signup")).pack()
-    Button(signUpFrame, text="Already have an account? Sign in", width=30, height=1, command=lambda: showFrame(signInFrame)).pack()
-
-def setUpMainMenuFrame():
-    Label(mainMenuFrame, text="MAIN MENU").pack()
-    Button(mainMenuFrame, text="List all cities", width=15, height=1).pack()
-    Button(mainMenuFrame, text="Select a city", width=15, height=1).pack()
-
-def connectServer(entry, port = 65432):
-    host = entry.get()
-    serverAddr = (host, port)
-    try:
-        s.connect(serverAddr)
-        print('Connected to server: ' + str(serverAddr))
-        showFrame(signInFrame)
-    except:
-        print("Could not find server's IP or request timeout")
-        messagebox.showerror("Error", "Could not find server's IP or request timeout")
-
-
+PORT = 65432
 root = Tk()
 root.geometry("400x400")
 root.title("Weathery App")
@@ -89,22 +125,24 @@ root.title("Weathery App")
 root.rowconfigure(0, weight=1)
 root.columnconfigure(0, weight=1)
 
+pbFrame = Frame(root)
 chooseSVFrame = Frame(root)
 signInFrame = Frame(root)
 signUpFrame = Frame(root)
 mainMenuFrame = Frame(root)
 
-for frame in (chooseSVFrame, signInFrame, signUpFrame, mainMenuFrame):
+for frame in (pbFrame, chooseSVFrame, signInFrame, signUpFrame, mainMenuFrame):
     frame.grid(row=0, column=0, sticky='nsew')
 
+# setUpPBFrame()
 setUpChooseSVFrame()
 setUpSignInFrame()
 setUpSignUpFrame()
 setUpMainMenuFrame()
-
 #receiveMsgList = {}
 
-showFrame(chooseSVFrame)
+Thread(target=showFrame, args=(chooseSVFrame,)).start()
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 root.mainloop()
--- server1.py
+++ server1.py
@@ -1,6 +1,7 @@
 #import libraries
 import socket
 import json
+from threading import Thread
 
 
 # functions
@@ -28,9 +29,9 @@ def checkLogIn(username, password, userData):
             return True
     return False
 
-def logInSection(conn, userData):
+def logInSection(client, userData):
     while True:
-        clientReq = conn.recv(1024)
+        clientReq = client.recv(1024)
         strclientReq = clientReq.decode("utf8")
         data = strclientReq.split()
         reqType = data[0]
@@ -41,14 +42,14 @@ def logInSection(conn, userData):
                 username = data[1]
                 password = data[2]
                 if checkLogIn(username, password, userData):
-                    conn.sendall(bytes("sign in success", "utf8"))
+                    client.sendall(bytes("sign in success", "utf8"))
                     print("sign in success")
                     break
                 else:
-                    conn.sendall(bytes("info incorrect", "utf8"))
+                    client.sendall(bytes("info incorrect", "utf8"))
                     print("info incorrect")
             else:
-                conn.sendall(bytes("syntax error", "utf8"))
+                client.sendall(bytes("syntax error", "utf8"))
                 print("syntax error")
 
         elif reqType == "signup":
@@ -58,22 +59,22 @@ def logInSection(conn, userData):
                 password = data[2]
                 if checkExistUsername(username, userData) == False:
                     createNewUser(username, password, userData)
-                    conn.sendall(bytes("sign up success", "utf8"))
+                    client.sendall(bytes("sign up success", "utf8"))
                     print("sign up success")
                     break
                 else:
-                    conn.sendall(bytes("username exists", "utf8"))
+                    client.sendall(bytes("username exists", "utf8"))
                     print("username exists")
             else:
-                conn.sendall(bytes("syntax error", "utf8"))
+                client.sendall(bytes("syntax error", "utf8"))
                 print("syntax error")
 
         else:
-            conn.sendall(bytes("unknown command", "utf8"))
+            client.sendall(bytes("unknown command", "utf8"))
             print("unknown command")
 
-def commandManager(strclientReq, weatherData):
-    commandArr = strclientReq.split()
+def commandManager(strClientReq, weatherData):
+    commandArr = strClientReq.split()
     if commandArr[0] == "/help":
         return "\n/city [city_name]\n[city_name]: TPHCM, HaNoi, DaNang, Hue"
     elif commandArr[0] == "/city":
@@ -87,13 +88,12 @@ def commandManager(strclientReq, weatherData):
     else:
         return "unknown command"
 
-
-def communicateSection(conn, weatherData):
+def communicateSection(client, weatherData):
     hint = "logged in"
-    conn.sendall(bytes(hint, "utf8"))
+    client.sendall(bytes(hint, "utf8"))
 
     while True:
-        clientReq = conn.recv(1024)
+        clientReq = client.recv(1024)
         strclientReq = clientReq.decode("utf8")
 
         if strclientReq == "/exit":
@@ -101,9 +101,25 @@ def communicateSection(conn, weatherData):
         if not clientReq:
             break
 
-        print(addr, strclientReq)
         serverResponse = commandManager(strclientReq, weatherData)
-        conn.sendall(bytes(serverResponse, "utf8"))
+        client.sendall(bytes(serverResponse, "utf8"))
+
+def processClientReq(client, clientAddr):
+    while True:
+        logInSection(client, userData)
+        communicateSection(client, weatherData)
+
+        client.close()
+        print(clientAddr, "has disconnected")
+
+def acceptClientConnections():
+    while True:
+        client, clientAddr = server.accept()
+        print(clientAddr, "has connected")
+        clientAdrrs[client] = clientAddr
+        Thread(target=processClientReq, args=(client, clientAddr,)).start()
+
+
 
 
 # main function
@@ -111,24 +127,20 @@ if __name__ == "__main__":
     HOST = '127.0.0.1'
     PORT = 65432
 
+    clientSockets = {}
+    clientAdrrs = {}
+
     weatherJson = open("weather.json")
     weatherData = json.load(weatherJson)
     userJson = open("user.json")
     userData = json.load(userJson)
 
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind((HOST, PORT))
-    s.listen(5)
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((HOST, PORT))
+    server.listen(5)
     print("Server 1 is online, wating for connections...")
 
-    while True:
-        conn, addr = s.accept()
-        print(addr, "has connected")
-
-        logInSection(conn, userData)
-        communicateSection(conn, weatherData)
-
-        conn.close()
-        print(addr, "has disconnected")
-
-    s.close()
+    threadAccept = Thread(target=acceptClientConnections)
+    threadAccept.start()
+    threadAccept.join()
+    server.close()

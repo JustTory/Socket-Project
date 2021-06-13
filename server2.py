a4d1b581edce0a825a87c81f419e35f0713d3909
kurozemi <tranngminhdao@gmail.com>
2021-06-13 21:44:23 +0700

    getTodayWeather

--- server2.py
+++ server2.py
@@ -2,10 +2,13 @@ import socket
 import json
 from datetime import date
 
+MONTHS = ["January", "Febuary", "May", "June"]
 today = date.today()
 
-DAY = today.strftime("%d")
+DAY = "13"
+# DAY = today.strftime("%d")
 MONTH = today.strftime("%B")
+YEAR = today.strftime("%Y")
 
 print(DAY)
 print(MONTH)
@@ -13,7 +16,10 @@ print(MONTH)
 HOST = '127.0.0.1' # server's IP
 PORT = 65432 # server's port: any non-privileged ports
 weatherJson = open("weather.json")
-weatherData = json.load(weatherJson)
+data = json.load(weatherJson)
+
+cityData = data["cities"]
+weatherData = data["weather"]
 
 # s = socket.socket(addr_family, type)
 # addr_family:
@@ -27,6 +33,60 @@ s.bind((HOST, PORT))
 s.listen(5)
 print("Server 2 is online, wating for connections...")
 
+def checkExistsCity(id):
+    try:
+        cityData[id]
+    except:
+        return False
+    return True
+
+def getCityWeather(city, numDay):
+    if (checkExistsCity(city) == False):  
+        return "This city doesn't exist"
+
+    res = "\n"
+    day_loop = DAY
+    month_loop = MONTH
+    year_loop = YEAR
+
+    if (int(DAY) - 6 < 0): 
+        index = MONTHS.index(MONTH)
+        PrevMonth = MONTHS[index-1] 
+        keys = list(weatherData[YEAR][PrevMonth])
+        lastDayofPrevMonth = keys[-1]
+
+    getNumofDay = numDay    
+    for i in range(getNumofDay):
+        try:
+            date_data = weatherData[year_loop][month_loop][day_loop]
+            if (date_data[city]):
+                weather = date_data[city]
+        except:
+            weather = "NaN"    
+
+        res += "%-5s %s, %s: %s\n" % (month_loop,day_loop.zfill(2), year_loop,weather)
+        day_loop = str(int(day_loop) - 1)
+        if (int(day_loop) <= 0):
+            day_loop = lastDayofPrevMonth
+            month_loop = PrevMonth   
+    return res
+def getWeatherAll(day = DAY, month = MONTH, year = YEAR): 
+    allCity = list(cityData)
+
+    try:
+        date_data = weatherData[year][month][day]
+    except:
+        return "No data available"
+    res = "\n[%s %s, %s]:\n" % (month, day, year)
+    for city in allCity:
+        try: 
+            status = date_data[city]
+        except:
+            status = "NaN" 
+        res += "%s: %s\n" % (city, status)
+    return res
+
+    
 while True:
     conn, addr = s.accept()
     print(addr, "has connected")
@@ -42,18 +102,13 @@ while True:
         print("Client: " + str_data)
         res = "NaN data"
         user_req = str_data.split()
-        if (user_req[0] == "/city"):
-            print("run in here 1")
-            for city in weatherData["cities"]:
-                print("run in here 2")
-                if (city["cityName"] == user_req[1]):
-                    print("run in here 3")
-                    res = "\n"
-                    for days in range (int(DAY) - 6,int(DAY) + 1):
-                        res += "%s %s: %s\n" % (MONTH,days, city["data"][MONTH][str(days)])
-                    break
 
-        conn.sendall(bytes(res, "utf8"))
+        if (user_req[0] == "/city"):
+            data_transfer = getCityWeather(user_req[1], 7)
+        if (user_req[0] == "/list"):
+            data_transfer = getWeatherAll()
+
+        conn.sendall(bytes(data_transfer, "utf8"))
 
     conn.close()
     print(addr, "has disconnected")
